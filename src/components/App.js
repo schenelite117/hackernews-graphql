8896142576ddf33879e88812ebd910bef610ac61
Stephen Chen <stephchen93@gmail.com>
2020-04-11 12:31:06 -0700

    subscriptions and pagination completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import LinkList from "./LinkList";
 import CreateLink from "./CreateLink";
-import Login from "./Login";
-import { Switch, Route } from "react-router";
 import Header from "./Header";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Login from "./Login";
 import Search from "./Search";
 
 class App extends Component {
@@ -13,14 +13,17 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
-            <Route exact path="/" component={LinkList} />
+            <Route exact path="/" render={() => <Redirect to="/new/1" />} />
             <Route exact path="/create" component={CreateLink} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/search" component={Search} />
+            <Route exact path="/top" component={LinkList} />
+            <Route exact path="/new/:page" component={LinkList} />
           </Switch>
         </div>
       </div>
     );
   }
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-
-// 1
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
@@ -12,8 +10,10 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { BrowserRouter } from "react-router-dom";
 import { setContext } from "apollo-link-context";
 import { AUTH_TOKEN } from "./constants";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
-// 2
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
@@ -28,8 +28,27 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
 
@@ -41,8 +60,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
